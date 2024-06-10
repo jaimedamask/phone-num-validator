b0799a4aad017e75e9f2f15dd36e10717e9c36ec
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-06-10 18:40:44 -0400

    Feat: Add checkInput function to check number input and update results accordingly

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ const clearBtn = document.getElementById('clear-btn');
 const results = document.getElementById('results-div');
 
 const numRegex = /^(1\s?)?(\([0-9]{3}\)|[0-9]{3})(\s|-)?([0-9]{3})(\s|-)?([0-9]{4})$/;
+
+const checkInput = () => {
+    const inputVal = input.value;
+
+    if (inputVal) {
+        if (numRegex.test(inputVal)) {
+            results.textContent = `Valid US number: ${inputVal}`;
+        } else {
+            results.textContent = `Invalid US number: ${inputVal}`;
+        }
+    } else {
+        alert('Please provide a phone number');
+    }
+}
