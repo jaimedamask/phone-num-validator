727696866ae8c6f97b092ecb6caaf307772372eb
jaimedamask <123277441+jaimedamask@users.noreply.github.com>
2024-06-10 18:24:05 -0400

    Feat: Add numRegex variable for testing number input

--- main.js
+++ main.js
@@ -2,3 +2,5 @@ const input = document.getElementById('user-input');
 const checkBtn = document.getElementById('check-btn');
 const clearBtn = document.getElementById('clear-btn');
 const results = document.getElementById('results-div');
+
+const numRegex = /^(1\s?)?(\([0-9]{3}\)|[0-9]{3})(\s|-)?([0-9]{3})(\s|-)?([0-9]{4})$/;
